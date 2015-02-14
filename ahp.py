7e5521bfc4327d6aec4a850d7ecb874aa6a39629
Antoine Neuenschwander <antoine@schoggi.org>
2015-02-14 00:33:18 +0000

    added db handling (broken atm)

--- ahp.py
+++ ahp.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import sqlite3 as lite
 
 app = Flask(__name__)
 
@@ -12,8 +13,22 @@ def index():
 
 @app.route('/survey/')
 def survey():
-	print "***: ",  request.args.get('natres_pubtrans', 'n/a')
 	return render_template('survey.html')
+
+@app.route('/done/')
+def done():
+#	save_record(**request.args)
+	#return "thank you"
+	s = ""
+	for key,value in request.args:
+		s += "%s = %s<br/>" % (key, value)
+	return s
+
+def save_record(**fields):
+	con = lite.connect('db/survey.db')
+	with con:
+		cur = con.cursor()
+		cur.execute("INSERT INTO survey (natres_pubtrans, natres_habitat, natres_housing) VALUES(?, ?, ?)", (float(fields["natres_pubtrans"]), float(fields["natres_habitat"]), float(fields["natres_housing"])))
 
 if __name__ == '__main__':
 	app.debug = True
---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE survey (
+  id INTEGER NOT NULL PRIMARY KEY,
+  ip_addr VARCHAR(250),
+  timestamp DATETIME,
+  natres_pubtrans REAL NOT NULL,
+  natres_habitat REAL NOT NULL,
+  natres_housing REAL NOT NULL
+);
